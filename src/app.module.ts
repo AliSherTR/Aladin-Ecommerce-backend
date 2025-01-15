10c3ab99af9b9c3c0a0e92307b6ea61c240155e6
AliSherTR <alisherkhan817@gmail.com>
2025-01-15 11:40:25 +0500

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule,  UsersModule, PrismaModule],
+  imports: [AuthModule,  UsersModule, PrismaModule , ConfigModule.forRoot() ],
   controllers: [],
   providers: [],
 })
